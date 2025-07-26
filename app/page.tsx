841acca74838be4a44fa7d8f71625b8b3ec4ad27
Ray Agni <jagni001@ucr.edu>
2025-07-26 03:55:58 -0700

    clean up main

--- app/page.tsx
+++ app/page.tsx
@@ -3,18 +3,18 @@
  */
 import { cookies } from 'next/headers';
 
-import DashboardSidebar from "@/components/dashboard/sidebar";
-import { Button } from "@/components/ui/button";
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import DashboardSidebar from "@/components/sidebar";
+import { SidebarProvider } from "@/components/ui/sidebar";
 
 export default async function Home() {
   const cookieStore = await cookies()
   const defaultOpen = cookieStore.get("sidebar_state")?.value === "true"
+
   return (
     <div>
       <SidebarProvider defaultOpen={defaultOpen}>
         <DashboardSidebar />
-        <main>
+        <main className="pl-2">
         </main>
       </SidebarProvider>
     </div>
