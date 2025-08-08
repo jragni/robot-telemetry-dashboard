328bced52e7e35d4018129ba98c71db20375bd3f
Jhensen Ray Agni <jhensenrayagni@gmail.com>
2025-08-08 01:40:37 -0700

    add toast and sidebar for connections

--- components/sidebar/definitions.ts
+++ components/sidebar/definitions.ts
@@ -2,7 +2,15 @@
  * sidebar definitions
  */
 
+import { ConnectionStatus } from "@/components//dashboard/definitions"
+
 export interface ConnectionDialogFormData {
   connectionName: string,
   webSocketUrl: string
+}
+
+export interface ConnectionsListItemProps {
+  id: string
+  name: string
+  status: ConnectionStatus
 }
