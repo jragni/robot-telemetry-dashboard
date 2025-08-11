69bbdd941081a2f729e504481e15f4d8f7108a6d
Jhensen Ray Agni <jhensenrayagni@gmail.com>
2025-08-10 23:49:32 -0700

    add control modal and section

---
+++ components/controlsection/AddControlModal.tsx
@@ -0,0 +1,93 @@
+"use client"
+
+import { useState } from "react";
+import { Plus } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Label } from "@/components/ui/label";
+import ComboBox from "@/components/combobox";
+import { getOptionsFromSubs } from "./helpers";
+import { RobotConnection } from "../dashboard/definitions";
+
+interface AddControlModalProps {
+  isDialogOpen: boolean
+  selectedConnection: RobotConnection | null
+  setIsDialogOpen: (isDialogOpen: boolean) => void
+}
+
+export default function AddControlModal({
+  isDialogOpen,
+  selectedConnection,
+  setIsDialogOpen,
+}: AddControlModalProps) {
+  const [isComboBoxOpen, setIsComboBoxOpen] = useState<boolean>(false);
+  const [formData, setFormData] = useState({
+    topic: '',
+  });
+
+  const {
+    subscriptions,
+    status: connectionStatus,
+  } = selectedConnection || { subscriptions: []};
+  const topicOptions = getOptionsFromSubs(subscriptions);
+
+  const handleUpdateForm = (field: string, value: unknown) => {
+    setFormData((prev) => ({
+      ...prev,
+      [field]: value,
+    }));
+  }
+  const isAddControlButtonDisabled = !selectedConnection || connectionStatus !== 'connected'
+
+  return (
+    <Dialog onOpenChange={() => isAddControlButtonDisabled ? null : setIsDialogOpen(!isDialogOpen)} open={isDialogOpen}>
+			<DialogTrigger
+        className="w-fit bg-orange-100"
+        disabled={isAddControlButtonDisabled}
+        asChild
+      >
+        <div>
+          <Button
+            variant="default"
+            disabled={isAddControlButtonDisabled}
+          >
+            <Plus />
+            <Label>Add a Control</Label>
+          </Button>
+        </div>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Add a Robot Control</DialogTitle>
+          <DialogDescription>
+            Send commands your robot by creating buttons that publish messages to selected topics
+          </DialogDescription>
+        </DialogHeader>
+        <div>
+          <Label
+            className="font-bold my-1"
+            htmlFor="topic-combobox"
+          >
+            Topic
+          </Label>
+          <ComboBox
+            id="topic-combobox"
+            open={isComboBoxOpen}
+            options={topicOptions}
+            setOpen={() => setIsComboBoxOpen(!isComboBoxOpen)}
+            value={formData.topic}
+            setValue={(val) => handleUpdateForm('topic', val)}
+          />
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+}
