8eb2faf116a0307782885cc99b6521575a506a52
Jhensen Ray Agni <jhensenrayagni@gmail.com>
2025-08-10 21:24:28 -0700

    refactor sidebar design

--- components/sidebar/ConnectionsListItem.tsx
+++ components/sidebar/ConnectionsListItem.tsx
@@ -1,17 +1,6 @@
-import {
-  Crosshair,
-  Plug,
-  Trash,
-  Unplug
-} from "lucide-react";
+import { Crosshair } from "lucide-react";
 
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion";
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button';
 
 import { ConnectionsListItemProps } from "./definitions";
 import ConnectionInformationDialog from "./ConnectionInformationDialog";
@@ -31,63 +20,27 @@ export default function ConnnectionListItem({
   url,
 }: ConnectionsListItemProps): React.ReactNode {
 
-  const buttonProps = status === 'connected'
-     ?{
-      'aria-label': `Disconnect from ${name}`,
-      className: 'bg-green-300 hover:bg-green-200 h-5 w-5',
-      onClick: handleDisconnect,
-      variant: 'destructive' as const,
-    } : {
-      'aria-label': `reconnect to ${name}`,
-      className: 'bg-red-500 hover:bg-red-300 h-5 w-5',
-      onClick: handleReconnect,
-      variant: 'secondary' as const,
-    };
 
   return (
-    <li>
-      <Accordion
-        className="w-full"
-        defaultValue="item-1"
-        collapsible
-        type="single"
-      >
-        <AccordionItem value="item-1">
-          <AccordionTrigger className="p-0">
-            <span className="font-semibold">{name}</span>
-          </AccordionTrigger>
-          <AccordionContent className="rounded-md bg-accent p-2">
-            <div className="items-center flex gap-2 flex-row-reverse">
-              <ConnectionInformationDialog
-                id={id}
-                name={name}
-                status={status}
-                url={url}
-              />
-              <Button
-                className="h-5 w-5 hover:opacity-70"
-                onClick={handleSelectConnection}
-              >
-                <Crosshair fill={isSelected ? "green" : ""} />
-              </Button>
-              <Button {...buttonProps}>
-                {status !== 'connected'
-                  ? <Unplug />
-                  : <Plug />
-                }
-              </Button>
-              <Button
-                aria-label="remove connecton"
-                className="h-5 w-5 hover:bg-orange-500 bg-orange-700"
-                onClick={handleRemoveConnection}
-                variant="destructive"
-              >
-                <Trash />
-              </Button>
-            </div>
-          </AccordionContent>
-        </AccordionItem>
-      </Accordion>
+    <li className="flex items-center justify-between">
+      <span className="font-semibold">{name}</span>
+      <div className="flex gap-2">
+          <Button
+            className="h-5 w-5 hover:opacity-70"
+            onClick={handleSelectConnection}
+          >
+            <Crosshair fill={isSelected ? "green" : ""} />
+          </Button>
+          <ConnectionInformationDialog
+            handleDisconnect={handleDisconnect}
+            handleReconnect={handleReconnect}
+            handleRemoveConnection={handleRemoveConnection}
+            id={id}
+            name={name}
+            status={status}
+            url={url}
+        />
+      </div>
     </li>
-  )
+  );
 }
